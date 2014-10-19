ffc2c9595fb52574bebcc621dc14cb35282dfb46
xc <xc@x>
2014-10-19 12:48:51 +0400

    templates_c

---
+++ templates_c/e43b807af9cc8df7d350c3baf9e47f167c9520a0.file.index.tpl.php
@@ -0,0 +1,80 @@
+<?php /* Smarty version Smarty-3.1.8, created on 2014-10-19 12:39:58
+         compiled from "index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:118324fc4be2a4e74f9-99203108%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'e43b807af9cc8df7d350c3baf9e47f167c9520a0' => 
+    array (
+      0 => 'index.tpl',
+      1 => 1413707993,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '118324fc4be2a4e74f9-99203108',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.8',
+  'unifunc' => 'content_4fc4be2a5ab1b9_46497891',
+  'variables' => 
+  array (
+    'table' => 0,
+  ),
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_4fc4be2a5ab1b9_46497891')) {function content_4fc4be2a5ab1b9_46497891($_smarty_tpl) {?><html>
+   <head>
+      <title>btocalc</title>
+      <link rel='StyleSheet' href='css.css' type='text/css' />
+      <meta http-equiv="Content-Type" content="text/html;  charset=utf-8">
+   </head>
+   <body>
+      <center>
+      <table>
+         <tr>
+            <td>
+               <table width=100%<?php ?>>
+                  <tr>
+                     <td><a href="<?php echo $_SERVER['REQUEST_URI'];?>
+">
+                        Ссылка на текущие расчеты</a>
+                     <td><a href="/">
+                        <?php echo $_SERVER['SERVER_NAME'];?>
+</a>
+                     <td><a href="
+                        /?set%5BTOBAPbI%5D%5B0%5D%5B0%5D=%CD%E8%E7%EA%EE%F1.&set%5BTOBAPbI%5D%5B0%5D%5B1%5D=20&set%5BTOBAPbI%5D%5B0%5D%5B2%5D=10&set%5BTOBAPbI%5D%5B1%5D%5B0%5D=%CE%E1%FB%F7%ED%FB%E9&set%5BTOBAPbI%5D%5B1%5D%5B1%5D=45&set%5BTOBAPbI%5D%5B1%5D%5B2%5D=25&set%5BCbIPbE%5D%5B0%5D%5B0%5D=Me%F2%E0%EB%EB&set%5BCbIPbE%5D%5B0%5D%5B1%5D=180&set%5BCbIPbE%5D%5B1%5D%5B0%5D=%D2%E5%EA%F1%F2%E8%EB%FC&set%5BCbIPbE%5D%5B1%5D%5B1%5D=200&set%5BCbIPbE%5D%5B2%5D%5B0%5D=%CF%F0%EE%E4%EE%E2.&set%5BCbIPbE%5D%5B2%5D%5B1%5D=150&set%5BCbIPbE%5D%5B3%5D%5B0%5D=%C4%F0%E5%E2%E5%F1.&set%5BCbIPbE%5D%5B3%5D%5B1%5D=66&set%5Bshop%5D%5B0%5D%5B0%5D=%CC%E0%E3%E0%E7%E8%ED+%B91&set%5Bshop%5D%5B0%5D%5B1%5D=2000&set%5Bshop%5D%5B1%5D%5B0%5D=%CC%E0%E3%E0%E7%E8%ED+%B92&set%5Bshop%5D%5B1%5D%5B1%5D=5000&set%5Bshop%5D%5B2%5D%5B0%5D=%CC%E0%E3%E0%E7%E8%ED+%B93&set%5Bshop%5D%5B2%5D%5B1%5D=30000&set%5BPACXODbI%5D%5B0%5D%5B0%5D=8&set%5BPACXODbI%5D%5B0%5D%5B1%5D=2&set%5BPACXODbI%5D%5B0%5D%5B2%5D=2&set%5BPACXODbI%5D%5B0%5D%5B3%5D=2&set%5BPACXODbI%5D%5B1%5D%5B0%5D=17&set%5BPACXODbI%5D%5B1%5D%5B1%5D=3&set%5BPACXODbI%5D%5B1%5D%5B2%5D=3&set%5BPACXODbI%5D%5B1%5D%5B3%5D=3&Submit=%CF%E5%F0%E5%F1%F7%E8%F2%E0%F2%FC
+                        ">
+                        Упрощенная версия</a>
+                     <td><a title="Используйте обозреватели Opera, Mozilla FireFox, Safari, Google Chrome ..." href="
+                        /?set%5BTOBAPbI%5D%5B0%5D%5B0%5D=%C8%F1%EA%EB%FE%F7%E8%F2%E5%EB%FC%ED%FB%E9&set%5BTOBAPbI%5D%5B0%5D%5B1%5D=3000&set%5BTOBAPbI%5D%5B0%5D%5B2%5D=250&set%5BTOBAPbI%5D%5B1%5D%5B0%5D=%DD%EB%E8%F2%ED%FB%E9&set%5BTOBAPbI%5D%5B1%5D%5B1%5D=5000&set%5BTOBAPbI%5D%5B1%5D%5B2%5D=300&set%5BTOBAPbI%5D%5B2%5D%5B0%5D=%CD%E5%F4%F2%FC&set%5BTOBAPbI%5D%5B2%5D%5B1%5D=500000&set%5BTOBAPbI%5D%5B2%5D%5B2%5D=0&set%5BTOBAPbI%5D%5B3%5D%5B0%5D=%C4%E8%E7%E5%EB%FC&set%5BTOBAPbI%5D%5B3%5D%5B1%5D=150000&set%5BTOBAPbI%5D%5B3%5D%5B2%5D=0&set%5BTOBAPbI%5D%5B4%5D%5B0%5D=%C1%E5%ED%E7%E8%ED+1+%F3%F0&set%5BTOBAPbI%5D%5B4%5D%5B1%5D=15000&set%5BTOBAPbI%5D%5B4%5D%5B2%5D=80&set%5BCbIPbE%5D%5B0%5D%5B0%5D=Me%F2%E0%EB%EB&set%5BCbIPbE%5D%5B0%5D%5B1%5D=200&set%5BCbIPbE%5D%5B1%5D%5B0%5D=%D2%E5%EA%F1%F2%E8%EB%FC&set%5BCbIPbE%5D%5B1%5D%5B1%5D=300&set%5BCbIPbE%5D%5B2%5D%5B0%5D=%CF%F0%EE%E4%EE%E2.&set%5BCbIPbE%5D%5B2%5D%5B1%5D=150&set%5BCbIPbE%5D%5B3%5D%5B0%5D=%C4%F0%E5%E2%E5%F1.&set%5BCbIPbE%5D%5B3%5D%5B1%5D=100&set%5BCbIPbE%5D%5B4%5D%5B0%5D=%CD%E5%F4%F2%FC&set%5BCbIPbE%5D%5B4%5D%5B1%5D=500000&set%5Bshop%5D%5B0%5D%5B0%5D=%CC%E0%E3%E0%E7%E8%ED+%B91&set%5Bshop%5D%5B0%5D%5B1%5D=20000&set%5Bshop%5D%5B1%5D%5B0%5D=%CC%E0%E3%E0%E7%E8%ED+%B92&set%5Bshop%5D%5B1%5D%5B1%5D=50000&set%5Bshop%5D%5B2%5D%5B0%5D=%CC%E0%E3%E0%E7%E8%ED+%B93&set%5Bshop%5D%5B2%5D%5B1%5D=300000&set%5BPACXODbI%5D%5B0%5D%5B0%5D=55&set%5BPACXODbI%5D%5B0%5D%5B1%5D=55&set%5BPACXODbI%5D%5B0%5D%5B2%5D=55&set%5BPACXODbI%5D%5B0%5D%5B3%5D=220&set%5BPACXODbI%5D%5B0%5D%5B4%5D=0&set%5BPACXODbI%5D%5B1%5D%5B0%5D=85&set%5BPACXODbI%5D%5B1%5D%5B1%5D=85&set%5BPACXODbI%5D%5B1%5D%5B2%5D=85&set%5BPACXODbI%5D%5B1%5D%5B3%5D=320&set%5BPACXODbI%5D%5B1%5D%5B4%5D=0&set%5BPACXODbI%5D%5B2%5D%5B0%5D=0&set%5BPACXODbI%5D%5B2%5D%5B1%5D=0&set%5BPACXODbI%5D%5B2%5D%5B2%5D=0&set%5BPACXODbI%5D%5B2%5D%5B3%5D=0&set%5BPACXODbI%5D%5B2%5D%5B4%5D=0&set%5BPACXODbI%5D%5B3%5D%5B0%5D=0&set%5BPACXODbI%5D%5B3%5D%5B1%5D=0&set%5BPACXODbI%5D%5B3%5D%5B2%5D=0&set%5BPACXODbI%5D%5B3%5D%5B3%5D=0&set%5BPACXODbI%5D%5B3%5D%5B4%5D=1&set%5BPACXODbI%5D%5B4%5D%5B0%5D=0&set%5BPACXODbI%5D%5B4%5D%5B1%5D=0&set%5BPACXODbI%5D%5B4%5D%5B2%5D=0&set%5BPACXODbI%5D%5B4%5D%5B3%5D=0&set%5BPACXODbI%5D%5B4%5D%5B4%5D=1&Submit=%CF%E5%F0%E5%F1%F7%E8%F2%E0%F2%FC
+                        ">
+                        Расширенная версия</a>
+                  <tr>
+                     <td colspan=4><input style="width: 340px;" value="http://<?php echo $_SERVER['SERVER_NAME'];?>
+<?php echo $_SERVER['REQUEST_URI'];?>
+">
+                        которую можно сохранить в закладках браузера
+               </table>
+               <form action="/" method="get" >
+                  <div style="float: left;"><?php echo $_smarty_tpl->tpl_vars['table']->value[0];?>
+</div>
+                  <div style="float: left;"><?php echo $_smarty_tpl->tpl_vars['table']->value[1];?>
+</div>
+                  <div style="float: left;"><?php echo $_smarty_tpl->tpl_vars['table']->value[2];?>
+</div>
+                  <div style="clear: left;"><?php echo $_smarty_tpl->tpl_vars['table']->value[3];?>
+</div>
+                  <div style="clear: left;">После ввода новых значений жми Enter на клавиатуре или эту кнопку <input type="submit" name="Submit" value="Пересчитать" /></div>
+               </form >
+               <div style="clear: left;">
+                  <?php echo $_smarty_tpl->tpl_vars['table']->value[4];?>
+
+                  <div style="float: left;  width: 50%;"><?php echo $_smarty_tpl->tpl_vars['table']->value[5];?>
+</div>
+                  <div style="float: right; width: 50%;"><?php echo $_smarty_tpl->tpl_vars['table']->value[6];?>
+</div>
+               </div>
+      </table>
+   </body>
+</html><?php }} ?>
